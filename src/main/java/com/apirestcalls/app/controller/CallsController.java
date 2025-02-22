b0bfef2395609d0ebd2a708a66910e17538c73bd
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-22 11:32:43 +0530

    [Arjun] Add.Greeting message with name as query

--- src/main/java/com/apirestcalls/app/controller/CallsController.java
+++ src/main/java/com/apirestcalls/app/controller/CallsController.java
@@ -2,6 +2,7 @@ package com.apirestcalls.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +11,14 @@ public class CallsController {
 
     //Creating GET Method
     @GetMapping("/hello")
-    public String SayHello(){
+    public String sayHello(){
         return "Hello From BridgeLabz";
+    }
+
+    //Create GET request method & pass name as query
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name){
+        return "Hello " + name + " From BridgeLabz";
     }
 
 }
